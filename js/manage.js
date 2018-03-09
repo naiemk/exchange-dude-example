a63b6c163938480d0b217c207c4b467aac97a982
nyeganeh <nyeganeh@bloomberg.net>
2018-03-09 00:15:39 -0500

    Basic functionality

--- js/manage.js
+++ js/manage.js
@@ -12,18 +12,18 @@
     iota = require("../iota/dist/iota")
 
     var state = {
-
+        weight: 3,
     }
 
     var self = this;
 
     $("#get-address-btn").click(function(e) {
         state.addresses = [];
-        self.seed = $("#get-address").val();
+        self.setSeed();
 
         // Deterministically generates a new address for the specified seed with a checksum
         for(var i=0; i<10; i++) {
-            var addr = iota.api._newAddress( self.seed, i, 2, true);
+            var addr = iota.api._newAddress( state.seed, i, 2, true);
             state.addresses.push(addr);
         }
         self.updateAddressHTML();
@@ -39,6 +39,10 @@
         $("#address-list").html(ht);
     }
 
+    $("#header").click(function(e){
+        iota.fe.newTestnetMilestome();
+    })
+
     $("#btc-claim-ext-address").click(function(e) {
         self.registerBtcAddress("btc")
     });
@@ -47,13 +51,19 @@
         self.registerBtcAddress("btc")
     });
 
+    self.setSeed = function() {
+        state.seed = $("#get-address").val();
+        state.seedTrytes = iota.pluginUtils.asciiToTrytes.toTrytes(self.seed);
+    }
+
     self.registerBtcAddress = function(prefix) {
+        self.setSeed();
         var extAddress = $("#" + prefix + "-ext-address").val();
         var intAddress = $("#" + prefix + "-int-address").val();
         console.log("Getting external addr", extAddress);
         var mappedAddress = iota.pluginUtils.externalToInternalAddress(extAddress); 
         console.log("Got external add converted ", mappedAddress);
-        iota.fe.sendExternalAddressClaim(self.seed, 4, 18, {address: mappedAddress, security:2, key:0}, intAddress, 
+        iota.fe.sendExternalAddressClaim(state.seed, 4, state.weight, {address: mappedAddress, security:2, key:0}, intAddress, 
             function(res) {
                 console.log("ExternalAddressClaim", res);
             });
